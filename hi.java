f641676e88a18be875c6d14e59bc243668e481b6
tlqkfcloud <108213590+tlqkfcloud@users.noreply.github.com>
2023-05-10 18:47:50 +0900

    Add files via upload

---
+++ hi.java
@@ -0,0 +1,15 @@
+package chap_02;
+
+
+public class hi{
+    public static void main(String[] args){
+        boolean play = true;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+        play = !play;
+        System.out.println(play);
+        
+    }
+}
